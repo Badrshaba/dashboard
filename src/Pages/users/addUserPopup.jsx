de37b397d9e4dfb3cce62d88636ada44c6a21335
AmrHesham <msam.team20@gmail.com>
2024-08-28 12:47:14 +0300

    Seprate Table For Users

---
+++ src/Pages/users/addUserPopup.jsx
@@ -0,0 +1,133 @@
+import { useRef } from 'react';
+import {
+  Button,
+  useDisclosure,
+  Modal,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  FormControl,
+  FormLabel,
+  Input,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  VStack,
+  Select,
+} from '@chakra-ui/react';
+import { Plus } from 'lucide-react';
+
+const AddUserPopup = ({ error, isLoading }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const cPasswordRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(
+      createNewUserFromDashboard({
+        name: usernameRef.current.value,
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+        password_confirmation: cPasswordRef.current.value,
+      })
+    );
+  };
+
+  return (
+    <>
+      <Button
+        colorScheme='teal'
+        leftIcon={<Plus />}
+        mt={5}
+        size='md'
+        onClick={onOpen}
+      >
+        Add Users
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader px={5} py={1}>Add User</ModalHeader>
+          <ModalCloseButton />
+          {error && (
+            <Alert status='error'>
+              <AlertIcon />
+              <AlertTitle>{error.response?.data?.data[0] || error.message}</AlertTitle>
+            </Alert>
+          )}
+          <form className='px-5 py-2'>
+            <VStack spacing={2}>
+              <FormControl>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  type='text'
+                  ref={usernameRef}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Email</FormLabel>
+                <Input
+                  type='email'
+                  ref={emailRef}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type='password'
+                  ref={passwordRef}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Confirm Password</FormLabel>
+                <Input
+                  type='password'
+                  ref={cPasswordRef}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Role</FormLabel>
+                <Select>
+                  <option
+                    value='user'
+                    selected
+                  >
+                    User
+                  </option>
+                  <option value='developer'>Developer</option>
+                  <option value='brookers'>Brookers</option>
+                </Select>
+              </FormControl>
+              {/* <FormControl>
+                <FormLabel>Address</FormLabel>
+                <Input type='text' ref={addressRef}/>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Phone</FormLabel>
+                <Input type='text' ref={phoneRef}/>
+              </FormControl> */}
+            </VStack>
+            <Button
+              colorScheme='teal'
+              className='w-full mt-4'
+              isLoading={isLoading}
+              type='submit'
+              onClick={(e) => handleSubmit(e)}
+            >
+              Submit
+            </Button>
+          </form>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default AddUserPopup;
--- src/Pages/users/users.jsx
+++ src/Pages/users/users.jsx
@@ -1,51 +1,16 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  Button,
-  useDisclosure,
-  Modal,
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  FormControl,
-  FormLabel,
-  Input,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-  VStack,
-  Select,
-  Text,
-} from '@chakra-ui/react';
-import { Plus } from 'lucide-react';
-import { createNewUserFromDashboard, getUsersAsync } from '../../redux/thunck/usersAsync';
-import TableComp from '../../componants/table-comp/table-comp';
-
+import { Text } from '@chakra-ui/react';
 import { Pagination } from 'antd';
+import { getUsersAsync } from '../../redux/thunck/usersAsync';
+import UsersTable from './table';
+import AddUserPopup from './addUserPopup';
 
 const Users = () => {
-  const [pageNumber, setPageNumber] = useState(1);
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const { users, isLoading, error } = useSelector((state) => state.users);
-  const usernameRef = useRef();
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const cPasswordRef = useRef();
   const dispatch = useDispatch();
+  const [pageNumber, setPageNumber] = useState(1);
   const tableHeading = ['id', 'username', 'email', 'role'];
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    dispatch(
-      createNewUserFromDashboard({
-        name: usernameRef.current.value,
-        email: emailRef.current.value,
-        password: passwordRef.current.value,
-        password_confirmation: cPasswordRef.current.value,
-      })
-    );
-  };
 
   useEffect(() => {
     dispatch(getUsersAsync(pageNumber));
@@ -54,97 +19,13 @@ const Users = () => {
   return (
     <div className='users-page bg-white p-5 rounded-md mt-5'>
       <h3 className='text-3xl'>Users</h3>
-      <Button
-        colorScheme='teal'
-        leftIcon={<Plus />}
-        mt={5}
-        size='md'
-        onClick={onOpen}
-      >
-        Add Users
-      </Button>
-
-      <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Add User</ModalHeader>
-          <ModalCloseButton />
-          {error && (
-            <Alert status='error'>
-              <AlertIcon />
-              <AlertTitle>{error.response?.data?.data[0] || error.message}</AlertTitle>
-            </Alert>
-          )}
-          <form className='p-5'>
-            <VStack spacing={4}>
-              <FormControl>
-                <FormLabel>Username</FormLabel>
-                <Input
-                  type='text'
-                  ref={usernameRef}
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel>Email</FormLabel>
-                <Input
-                  type='email'
-                  ref={emailRef}
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel>Password</FormLabel>
-                <Input
-                  type='password'
-                  ref={passwordRef}
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel>Confirm Password</FormLabel>
-                <Input
-                  type='password'
-                  ref={cPasswordRef}
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel>Role</FormLabel>
-                <Select>
-                  <option
-                    value='user'
-                    selected
-                  >
-                    User
-                  </option>
-                  <option value='developer'>Developer</option>
-                  <option value='brookers'>Brookers</option>
-                </Select>
-              </FormControl>
-              {/* <FormControl>
-                <FormLabel>Address</FormLabel>
-                <Input type='text' ref={addressRef}/>
-              </FormControl>
-              <FormControl>
-                <FormLabel>Phone</FormLabel>
-                <Input type='text' ref={phoneRef}/>
-              </FormControl> */}
-            </VStack>
-            <Button
-              colorScheme='teal'
-              className='w-full mt-4'
-              isLoading={isLoading}
-              type='submit'
-              onClick={(e) => handleSubmit(e)}
-            >
-              Submit
-            </Button>
-          </form>
-        </ModalContent>
-      </Modal>
-      <TableComp
-        headings={tableHeading}
-        data={users?.data}
+      <AddUserPopup
+        error={error}
+        isLoading={isLoading}
+      />
+      <UsersTable
+        tableHeadings={tableHeading}
+        users={users?.data}
       />
       <Pagination
         defaultCurrent={1}

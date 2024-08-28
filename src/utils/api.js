1d75d956974e6bfa7ebd0a43af2ba4e7a037bba6
Badrshaba <badrshaban1212@gmail.com>
2024-08-28 16:14:50 +0300

    app_key

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'https://ai.w-manage.org/api',
-  headers: {
-    Accept: 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('userToken')}`.replaceAll('"', ''),
-    APP_KEY: import.meta.env.VITE_APP_KEY,
-  },
+  
+  // headers: {
+  //   Accept: 'application/json',
+  //   Authorization: `Bearer ${localStorage.getItem('userToken')}`.replaceAll('"', ''),
+  //   APP_KEY: import.meta.env.VITE_APP_KEY,
+  // },
 });
 
 export const apiRegister = axios.create({

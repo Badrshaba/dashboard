26b2d4c1a5463f5c76275eba2378540386a7e476
AmrHesham <msam.team20@gmail.com>
2024-09-01 17:51:18 +0300

    Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,11 +4,8 @@ import properitesReducer from './slices/properites';
 import usersReducers from './slices/users';
 import compoundReducers from './slices/compounds';
 import bunnersReducers from './slices/bunners';
-<<<<<<< HEAD
-import featuresReducers from './slices/features'
-=======
+import featuresReducers from './slices/features';
 import subCategoriesReducers from './slices/subCategories';
->>>>>>> a5a2aace7e14106ccc0a937790934bcd008b1191
 
 const store = configureStore({
   reducer: {
@@ -17,11 +14,8 @@ const store = configureStore({
     users: usersReducers,
     compounds: compoundReducers,
     bunners: bunnersReducers,
-<<<<<<< HEAD
-    features: featuresReducers
-=======
+    features: featuresReducers,
     subCategories: subCategoriesReducers,
->>>>>>> a5a2aace7e14106ccc0a937790934bcd008b1191
   },
 });
 

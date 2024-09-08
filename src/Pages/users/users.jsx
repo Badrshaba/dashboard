b9bfb0c8b739bc0b36f59f02bc06191c89e9677f
AmrHesham <msam.team20@gmail.com>
2024-09-08 17:53:09 +0300

    Test

--- src/Pages/users/users.jsx
+++ src/Pages/users/users.jsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getUsersAsync } from '../../redux/thunck/usersAsync';
 import TestTable from './TestTable';
 import AddUserPopup from './addUserPopup';
-import useSelection from 'antd/es/table/hooks/useSelection';
 
 const Users = () => {
   const { error, isLoading } = useSelector((state) => state.users);
@@ -12,7 +11,7 @@ const Users = () => {
     dispatch(getUsersAsync(1));
   }, []);
   return (
-    <div className='bg-white p-3 rounded-md'>
+    <div className='px-8 py-5 bg-white'>
       <h3 className='text-3xl'>Users</h3>
       <div className=' flex justify-between items-center'>
         <AddUserPopup

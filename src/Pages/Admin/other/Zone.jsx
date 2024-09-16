fc3f56549f37e6a72f882da6a15295b30e2a2d12
Badrshaba <badrshaban1212@gmail.com>
2024-09-16 14:07:21 +0300

    test

--- src/Pages/Admin/other/Zone.jsx
+++ src/Pages/Admin/other/Zone.jsx
@@ -1,53 +1,123 @@
-import { Button, FormControl, FormLabel, Input, Modal, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, useDisclosure, VStack } from "@chakra-ui/react"
+import { Button, FormControl, FormLabel, Input, Modal, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Spinner, useDisclosure, VStack } from "@chakra-ui/react"
 import { Plus } from "lucide-react"
 import { DeleteAlert } from "../../../componants"
 import OthersCard from "../../../componants/others/OthersCard"
+import { useEffect, useState } from "react"
+import { api } from "../../../utils/api"
 
 const Zone = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-
+  const [Zones,setZones] = useState([])
+  const [isLoading,setIsLoading] = useState(false)
+  const [formData,setFormData] = useState({
+    name_en:"",
+    name_ar:""
+  })
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const getAllZones = async()=>{
+try {
+  let {data} = await api.get('/zones')
+  setZones(data?.data)
+} catch (error) {
+  console.log(error);
+}
+  }
+  const onSubmit = async(e) =>{
+    e.preventDefault();
+    setIsLoading(true)
+    console.log(formData);
+    try {
+      let {data} = await api.post('/zones',formData)
+      setZones(data?.data)
+      getAllZones()
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false)
+    setTimeout(()=>{
+      onClose()
+    },500)
+    for (let key in formData) {
+      if (formData.hasOwnProperty(key)) {
+        setFormData((prevData) => ({
+          ...prevData,
+          [key]: '',
+        }))
+      }
+    }
+  }
+  const deleteZone = async(e)=>{
+    try {
+      let {data} = await api.delete(`/zones/${e.id}`)
+      console.log(data?.data)
+      getAllZones()
+    } catch (error) {
+      console.log(error);
+    }
+  }
+useEffect(()=>{
+getAllZones()
+},[])
   return (
     <div>
-        <h3 className=' text-xl fw-semibold text-[#4D5454] ml-3' > Zone Details</h3>
-<div className=" flex space-x-3 ml-3 mt-3 " >
+        <h3 className=' text-xl fw-semibold text-[#4D5454] ml-3' > Zone </h3>
+        <Button
+        colorScheme='teal'
+        leftIcon={<Plus />}
+        mt={2}
+        size='md'
+        onClick={onOpen}
+        ml={3}
+      >
+        Add Zone
+      </Button>
+<div className=" flex flex-col space-y-3 space-x-3 ml-3 mt-3 " >
+{!!Zones.length?Zones.map((zone)=>(
+<OthersCard key={zone.id} obj={zone} deleteFun={deleteZone} />
+)):<Spinner/>}
 
-<OthersCard text={'A1'} />
-<OthersCard text={'A2'} />
-<div onClick={onOpen} className=" border bg-white border-black rounded-lg p-4 hover:cursor-pointer hover:bg-teal-600 hover:text-white transition-colors duration-75" >
-    <Plus size={25} strokeWidth={3}/>
-</div>
-<Modal isOpen={isOpen} onClose={onClose}>
+<Modal isOpen={isOpen} onClose={onClose} >
         <ModalOverlay />
-        
         <ModalContent>
           <ModalHeader>Add Zone</ModalHeader>
           <ModalCloseButton />
-          {/* {error && (
-            <Alert status='error'>
-              <AlertIcon />
-              <AlertTitle>{error?.message}</AlertTitle>
-            </Alert>
-          )} */}
-          <form className='px-5 py-2'>
+          <form className='px-5 py-2' onSubmit={onSubmit}>
             <VStack spacing={2}>
               <FormControl>
-                <FormLabel>Zone</FormLabel>
+                <FormLabel>Name :</FormLabel>
                 <Input
                   type='text'
-                //   ref={usernameRef}
+              name="name_en"
+              value={formData.name_en}
+              onChange={handleChange}
+              />
+              </FormControl>
+              <FormControl  style={{direction:'rtl'}}>
+                <FormLabel  > الاسم :</FormLabel>
+                <Input
+                  type='text'
+                  name="name_ar"
+                  value={formData.name_ar}
+                  onChange={handleChange}
                 />
               </FormControl>
             </VStack>
             <Button
               colorScheme='teal'
               className='w-full mt-4'
-            //   isLoading={isLoading}
+               isLoading={isLoading}
               type='submit'>
               Submit
             </Button>
           </form>
         </ModalContent>
-      </Modal>
+</Modal>
 </div>
     </div>
   )
--- src/componants/others/OthersCard.jsx
+++ src/componants/others/OthersCard.jsx
@@ -1,25 +1,69 @@
-import { useDisclosure } from '@chakra-ui/react';
-import React from 'react'
-import DeleteAlert from '../deleteAlert/DeleteAlert';
+import {  Button as AButton,FormControl, FormLabel, Input, Modal, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, useDisclosure, VStack } from "@chakra-ui/react";
+import React from "react";
+import DeleteAlert from "../deleteAlert/DeleteAlert";
+import { Edit, Trash } from "lucide-react";
+ import { Button } from "antd";
 
-const OthersCard = ({text}) => {
+const OthersCard = ({ obj,deleteFun }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen:isOpenModal, onOpen:onOpenModal, onClose:onCloseModal } = useDisclosure();
 
   return (
     <>
-    <div onClick={onOpen} className=" border bg-white border-black rounded-lg p-4 min-w-16 flex justify-center hover:text-white hover:font-bold hover:bg-red-700 hover:cursor-pointer transition-colors duration-75">
-    <h2>{text}</h2>
-    </div>
-    <DeleteAlert 
-        // userInfo={userInfo}
-        // deleteFun={deleteCompound}
+      <div
+        className=" w-4/5  space-x-3 border bg-white border-black rounded-lg p-4 min-w-16 flex justify-between font-bold  hover:cursor-pointer transition-colors duration-75"
+      >
+        <h2 className=" text-2xl">{obj?.name}</h2>
+        <div className=" flex space-x-3 ">
+          <button  onClick={onOpenModal}  className=" px-3 rounded-md bg-yellow-400 hover:bg-yellow-500"  >
+            <Edit />
+          </button>
+          <button  onClick={onOpen}  className=" px-3 rounded-md bg-red-600 hover:bg-red-700 " >
+            <Trash />
+          </button>
+        </div>
+      </div>
+      <DeleteAlert
+         userInfo={obj}
+         deleteFun={deleteFun}
         onClose={onClose}
         isOpen={isOpen}
-        head={`Delete ${text}`}
-        body={`Do you want delete ${text}`} 
-         />
+        head={`Delete `}
+        body={`Do you want delete `}
+      />
+      <Modal isOpen={isOpenModal} onClose={onCloseModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add Zone</ModalHeader>
+          <ModalCloseButton />
+          <form className='px-5 py-2'>
+            <VStack spacing={2}>
+              <FormControl>
+                <FormLabel>Name</FormLabel>
+                <Input
+                  type='text'
+               value={obj.name}                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Zone</FormLabel>
+                <Input
+                  type='text'
+                //   ref={usernameRef}
+                />
+              </FormControl>
+            </VStack>
+            <AButton
+              colorScheme='teal'
+              className='w-full mt-4'
+            //   isLoading={isLoading}
+              type='submit'>
+              Submit
+            </AButton>
+          </form>
+        </ModalContent>
+      </Modal>
     </>
-  )
-}
+  );
+};
 
-export default OthersCard
+export default OthersCard;
